372a5cfe927a2779c8469071d4bdad5fc77fb0ae
earthToAlice <girlofthedoodles@gmail.com>
2019-09-11 21:02:46 -0400

    update main

--- Main.java
+++ Main.java
@@ -14,11 +14,51 @@ public class Main
         Scanner s = new Scanner(System.in);
         
         System.out.print("Please enter 'B' to convert to Binary or 'D' to convert to Denary: ");
+        String convType = s.nextLine().toLowerCase();
         
-        if (s.nextLine().equalsIgnoreCase(""))
+        do
         {
-            
+            if (convType.equals("b"))
+            {
+                do
+                {
+                System.out.print("Please enter a number up to 255 to convert to binary: ");
+                while (!s.hasNextInt())
+                {
+                    System.out.println("Please enter a valid number.");
+                    s.next();
+                }
+                int input = s.nextInt();
+
+                }
+                while (input > 255);
+                
+                toBinary(input);
+
+            } //if(converting to binary)
+            else if (convType.equals("d"))
+            {
+                do
+                {
+                System.out.print("Please enter a number up to 11111111 to convert to denary: ");
+                while (!s.hasNextInt())
+                {
+                    System.out.println("Please enter a valid number.");
+                    s.next();
+                }
+                int input = s.nextInt();
+
+                }
+                while (input > 11111111);
+                
+                toDenary(input);
+
+            } //else if(converting to denary)
+
         }
+        while (!convType.equals("b") && !convType.equals("d"));
+        
+
     } //main(String[] args)
 
 } //Main
