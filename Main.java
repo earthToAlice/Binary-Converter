3e700ef3ac989c9b3300bcab0cdfbd32f11129f6
earthToAlice <girlofthedoodles@gmail.com>
2019-09-11 21:28:30 -0400

    fix logic that was dumb as shit

--- Main.java
+++ Main.java
@@ -19,50 +19,57 @@ public class Main
 
     static void typeToConvertTo(Scanner s)
     {
-        System.out.print("Please enter 'B' to convert to Binary or 'D' to convert to Denary: ");
-        String convType = s.nextLine().toLowerCase();
-        
         do
         {
-            if (convType.equals("b"))
+            System.out.print("Please enter 'B' to convert to Binary or 'D' to convert to Denary: ");
+
+            while (!s.hasNextLine())
             {
-                do
-                {
-                System.out.print("Please enter a number up to 255 to convert to binary: ");
-                while (!s.hasNextInt())
-                {
-                    System.out.println("Please enter a valid number.");
-                    s.next();
-                }
-                int input = s.nextInt();
+                System.out.println("Please enter a valid letter.");
+            }
 
-                }
-                while (input > 255);
-                
-                toBinary(input);
-
-            } //if(converting to binary)
-            else if (convType.equals("d"))
-            {
-                do
-                {
-                System.out.print("Please enter a number up to 11111111 to convert to denary: ");
-                while (!s.hasNextInt())
-                {
-                    System.out.println("Please enter a valid number.");
-                    s.next();
-                }
-                int input = s.nextInt();
-
-                }
-                while (input > 11111111);
-                
-                toDenary(input);
-
-            } //else if(converting to denary)
+            String convType = s.nextLine().toLowerCase();
 
         }
         while (!convType.equals("b") && !convType.equals("d"));
+        
+        
+        if (convType.equals("b"))
+        {
+            do
+            {
+            System.out.print("Please enter a number up to 255 to convert to binary: ");
+            while (!s.hasNextInt())
+            {
+                System.out.println("Please enter a valid number.");
+                s.next();
+            }
+            int input = s.nextInt();
+
+            }
+            while (input > 255);
+            
+            toBinary(input);
+
+        } //if(converting to binary)
+        else if (convType.equals("d"))
+        {
+            do
+            {
+            System.out.print("Please enter a number up to 11111111 to convert to denary: ");
+            while (!s.hasNextInt())
+            {
+                System.out.println("Please enter a valid number.");
+                s.next();
+            }
+            int input = s.nextInt();
+
+            }
+            while (input > 11111111);
+            
+            toDenary(input);
+
+        } //else if(converting to denary)
 
     } //typeToConvertTo(Scanner s)
 
